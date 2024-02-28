e7932c84b221c633418368546a4483e6a93ee102
atultirkey <114208817+atultirkey@users.noreply.github.com>
2024-02-28 11:47:58 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -11,26 +11,5 @@ class Node {
 };
 
 int main() {
-  Node* head;
-  Node* one = NULL;
-  Node* two = NULL;
-  Node* three = NULL;
-
-  one = new Node();
-  two = new Node();
-  three = new Node();
-
-  one->value = 1;
-  two->value = 2;
-  three->value = 3;
-
-  one->next = two;
-  two->next = three;
-  three->next = NULL;
-
-  head = one;
-  while (head != NULL) {
-    cout << head->value;
-    head = head->next;
-  }
+ cout<<"Atul tirkey 21BCS5094"<<endl;
 }
